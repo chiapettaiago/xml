c0f4bf0ab78d6071cf5a3c7f64a6c23eb2fd4162
Iago Chiapetta <iagochiapetta@gmail.com>
2024-05-08 11:52:50 -0300

    somatorio corrigido0

--- corrector/xml_corrector.py
+++ corrector/xml_corrector.py
@@ -6,9 +6,24 @@ class XMLCorrector:
     def modificar_xml(xml_string):
         try:
             root = etree.fromstring(xml_string)
+            
+            # Encontra todas as tags 'ans:guiaSP-SADT'
+            guias_sadt = [elem for elem in root.iter() if elem.tag.endswith('guiaSP-SADT')]
+            
+            for guia_sadt in guias_sadt:
+                # Soma todos os valores nas tags 'valorTotal' dentro da mesma 'ans:guiaSP-SADT'
+                valor_total = sum(float(elem.text) for elem in guia_sadt.iter() if elem.tag.endswith('valorTotal') and elem.text.strip())
+                
+                # Atualiza o valor na tag 'valorTotalGeral'
+                valor_total_geral = next((elem for elem in guia_sadt.iter() if elem.tag.endswith('valorTotalGeral')), None)
+                if valor_total_geral is not None:
+                    valor_total_geral.text = "{:.2f}".format(valor_total)
+            
+            # Corrige os números de 4 casas decimais
             for element in root.iter():
                 if element.text:
                     XMLCorrector._corrigir_numeros_4_casas(element)
+            
             return etree.tostring(root, encoding='utf-8', method='xml').decode("utf-8")
         except etree.XMLSyntaxError:
             return "Erro XML: XML inválido."
